622cc0214d4a09516a9be9e3bbfa6836e6850ded
Robert Chavers <56808029+rchavers@users.noreply.github.com>
2019-11-10 22:07:33 -0500

    Update backup-tablo.sh

--- backup-tablo.sh
+++ backup-tablo.sh
@@ -8,32 +8,35 @@
 ## requirements to use this script:
 ##   bash 4.4 or greater
 ## external utilities:
-##   ts, find, rm, tail, kill, ps, lftp, awk, ls, mkdir, mv
+##   ts, find, rm, tail, kill, ps, lftp, awk, ls, mkdir, mv, test, touch
 
-## restore command example:
+## restore command example: (note: rsync needs the slash after the source path)
+##
 ## mkdir /tmp/sdX1
 ## mount /dev/sdX1 /tmp/sdX1
 ## rsync -a --info=progress2 --no-inc-recursive /mnt/backups/tablo/rec/ /tmp/sdX1/rec/
 ## umount /tmp/sdX1
 
+## example cron entry
+##
+## 2am every day -- backup tablo recordings
+## 0	2	*	*	*	/usr/local/bin/backups/backup-tablo.sh --log
+
+
+
 ##
 ## variables you will most likely need to change
 ##
 tablo_ip_or_host="192.168.0.32"			#ip address (or hostname) of tablo device
-path_backups="/mnt/backups/tablo/rec"		#where to store your tablo shows
-path_deleted="/mnt/backups/tablo/deleted"	#where to store your deleted tablo shows
-path_logfile="/mnt/backups/tablo/log/backup-tablo.log"		#the location of the log file
+path_tablo="/mnt/backups/tablo"			#path to your backup location (you needs lots of free space :-)
 
-path_pidfile="/tmp/backup-tablo.pid"			#the temporary pid file while the program is running
-max_log_lines=1000				#number of old lines to keep in path_logfile before the program runs
-log_to_file=0					#set to 1 to always log to path_logfile (or call program with --log argument)
+path_backups="${path_tablo}/rec"		#where to store your tablo shows
+path_deleted="${path_tablo}/deleted"		#where to store your deleted tablo shows
+logfile="${path_tablo}/backup-tablo.log"	#where to store your logfile
+pidfile="/tmp/backup-tablo.pid"			#the temporary pid file while the program is running
+max_log_lines=1000				#number of old lines to keep in logfile before the program runs
+log_to_file=0					#set to 1 to always log to logfile (or call program with --log argument)
 days_to_keep_deleted="14"			#use 0 for unlimited days
-
-
-## example cron entry
-##
-#2am every day -- backup tablo recordings
-#0	2	*	*	*	/usr/local/bin/backups/backup-tablo.sh --log
 
 
 ##
@@ -54,7 +57,7 @@ need_to_delete=()
 while [ "$1" != "" ]; do
   case $1 in
     -l | --log )
-      # log output to $path_logfile
+      # log output to $logfile
       log_to_file=1
       ;;
   esac
@@ -65,8 +68,8 @@ done
 # first time running this program can take days since tablo only has 10/100 ethernet
 # subsequent running is much quicker.  you can safely stop and restart this program
 #
-if [ -e $path_pidfile ]; then
-  pid="$(<${path_pidfile})"
+if [ -e $pidfile ]; then
+  pid="$(<${pidfile})"
   # kill -0 just checks if we *can* kill, it does not send a kill signal
   # basically, this is just testing to see if the process is really running
   if /bin/kill -0 &>1 > /dev/null ${pid}; then
@@ -75,30 +78,40 @@ if [ -e $path_pidfile ]; then
     echo "WARNING: $0 (PID ${pid}) was started ${runtime} seconds ago; will not continue."
     exit 0
   else
-    #not already running, so remove orphan path_pidfile
-    /bin/rm ${path_pidfile}
+    #not already running, so remove orphan pidfile
+    /bin/rm ${pidfile}
   fi
 fi
-trap "/bin/rm ${path_pidfile}; exit" INT TERM EXIT
-echo $$ >${path_pidfile}
-
-# prune the path_logfile to a max size of $max_log_lines
-#
-if (( ${log_to_file} )); then
-  echo "$(/usr/bin/tail -n $max_log_lines $path_logfile)" > $path_logfile
-fi
-
+trap "/bin/rm ${pidfile}; exit" INT TERM EXIT
+echo $$ >${pidfile}
 
 
 ##
 ## function definitions
 ##
 
+initialize () {
+  # ensure paths are accessable and logfile is writable
+  /usr/bin/test -d ${path_tablo} || /bin/mkdir -p ${path_tablo}
+  /usr/bin/test -d ${path_tablo} || (echo "${path_tablo} could not be created.  Can not continue" && exit 1)
+  /usr/bin/test -d ${path_backups} || /bin/mkdir ${path_backups}
+  /usr/bin/test -d ${path_backups} || (echo "${path_backups} could not be created.  Can not continue" && exit 1)
+  /usr/bin/test -d ${path_deleted} || /bin/mkdir ${path_deleted}
+  /usr/bin/test -d ${path_deleted} || (echo "${path_deleted} could not be created.  Can not continue" && exit 1)
+  /usr/bin/test -w ${logfile} || /usr/bin/touch ${logfile}
+  /usr/bin/test -w ${logfile} || (echo "${logfile} could not be created.  Can not continue" && exit 1)
+
+  # prune the logfile to a max size of $max_log_lines
+  if (( ${log_to_file} )); then
+    echo "$(/usr/bin/tail -n $max_log_lines $logfile)" > $logfile
+  fi
+}
+
 mylog () {
   # simple log funtion to capture date and time in output
   tstamp=$(echo|/usr/bin/ts '[%Y-%m-%d %H:%M:%S]')
   if (( ${log_to_file} )); then
-    echo "${tstamp}${1}" >> $path_logfile
+    echo "${tstamp}${1}" >> $logfile
   else
     echo "${tstamp}${1}"
   fi
@@ -164,7 +177,7 @@ backup_tablo () {
   # mirror the /pvr/ directory from the tablo webserver to our local directory
   lftp_cmd="open http://${tablo_ip_or_host}:18080; mirror --verbose=1 --exclude [^/]+/log/$ --exclude [^/]+/tmp/$ --delete /pvr/ ${path_backups}"
   if (( $log_to_file )); then
-    /usr/bin/lftp -c "${lftp_cmd}" 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> $path_logfile
+    /usr/bin/lftp -c "${lftp_cmd}" 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> $logfile
   else
     /usr/bin/lftp -c "${lftp_cmd}" 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]'
   fi
@@ -175,6 +188,7 @@ backup_tablo () {
 ##
 ## main program code
 ##
+initialize
 
 mylog "----------started----------"
 
